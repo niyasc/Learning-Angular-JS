dd09153d615414c0c1c4d4e40f023042d1184d22
Niyas C <niyasmonc@gmail.com>
2016-01-19 23:40:07 +0530

    Completed json file creation. json read is succesful

--- quiz/js/quiz.js
+++ quiz/js/quiz.js
@@ -1,5 +1,16 @@
 (function(){
+	var app = angular.module('myQuiz', []);
 
-	// script goes here
 
+	app.controller('QuizController', ['$scope', '$http', '$sce', function($scope, $http, $sce){
+		$scope.score = 0;
+		$scope.activeQuestion = -1;
+		$scope.activeQuestionAnswered = 0;
+		$scope.percentage = 0;
+
+		$http.get('quiz_data.json').success(function(quizData) {
+			$scope.myQuestions = quizData;
+			$scope.totalQuestions = $scope.myQuestions.length;
+		});
+	}]);
 })();
