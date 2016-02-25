42146bad89c580591184810b3e3c2bed3f0b82ec
Niyas C <niyasmonc@gmail.com>
2016-02-25 06:45:01 +0000

    Commiting to debug on gh-pages

--- LoginFunction/scripts/app.js
+++ LoginFunction/scripts/app.js
@@ -1,27 +1,50 @@
-var app = angular.module("main", ['angularBetterPlaceholder', 'ngMockE2E']);
+var app = angular.module("main", ['angularBetterPlaceholder', 'ngMockE2E', 'ngRoute']);
 
-app.run(function ($httpBackend) {
+app.run(function($httpBackend) {
 
-    $httpBackend.whenPOST('/login').respond(function (method, url, data) {
+    $httpBackend.whenPOST('/login').respond(function(method, url, data) {
         var details = angular.fromJson(data);
         if (details.email == "test@test.com" && details.password == "test")
-            return [200, { loggedIn: true, userId: 'user123' }, {}];
+            return [200, {
+                loggedIn: true,
+                userId: 'user123'
+            }, {}];
         else
-            return [200, { loggedIn: false }, {}];
+            return [200, {
+                loggedIn: false
+            }, {}];
     });
 
 });
 
-app.controller("formCtrl", ['$scope', '$http', function ($scope, $http) {
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $routeProvider.when('/login', {
+        templateUrl: 'partials/login.html',
+        controller: 'LoginController'
+    }).otherwise({
+        redirectTo: '/login'
+    });
+    
+    $locationProvider.html5Mode(true);
+}]);
+
+app.controller("LoginController", ['$scope', '$http', function($scope, $http) {
     $scope.data = {};
     $scope.loading = false;
-    $scope.submit = function () {
+    $scope.postResult = 0;
+
+    $scope.submit = function() {
         $scope.loading = true;
-        var i = 1000;
-        while(i-- > 0);
-        $http.post('/login', $scope.data).success(function (data) {
-            console.log("hi" + data);
+        $http.post('/login', $scope.data).success(function(data) {
+            if (data.loggedIn == true)
+                $scope.postResult = 1;
+            else
+                $scope.postResult = 2;
         });
         $scope.loading = false;
     };
+}]);
+
+app.controller("MainController", ['$scope', function($scope) {
+
 }]);
